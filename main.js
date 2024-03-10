7c74b98d1fb6b27fcf432a849db14a6cec92988e
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-10 13:21:58 +0530

    added search feature

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import {sampleData} from "./sampleData.js";
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
   
@@ -18,24 +19,65 @@ document.addEventListener("DOMContentLoaded", (event) => {
             })
         }
     })
-const handleClick = ()=>{
-    var fname = document.getElementById('fname').value
-    var lname = document.getElementById('lname').value
-    var roll = document.getElementById('roll').value
-    var age = document.getElementById('age').value
-    var gender = document.getElementById('gen').value
-    var hobby = document.getElementById('hobby').value
-    const studentObj = {
-        firstName: fname,
-        lastLame: lname,
-        rollNumber: roll,
-        age: age,
-        gender: gender,
-        hobby: splitString(hobby)
+    const fillResponse = (studentObj) => {
+        const responseDiv = document.getElementById('response');
+        responseDiv.innerHTML = '';
+        if(studentObj){
+            for (const [key, value] of Object.entries(studentObj)) {
+                const newDiv = document.createElement('div');
+                newDiv.textContent= `${key} : ${value}`
+                responseDiv.appendChild(newDiv)
+            }
+        }
+        else{
+            const newDiv = document.createElement('div');
+            newDiv.textContent= `No such entry found!!!!`
+            responseDiv.appendChild(newDiv)
+        }
     }
-    console.log(studentObj)
+    
+const handleSearch=()=>{
+    let rollno=document.getElementById('searchname').value
+    const reqData = sampleData.find(student=> student.rollNumber==rollno)
+    console.log(reqData)
+    fillResponse(reqData)
+}
+const handleSwitch= ()=>{
+    let cn = document.getElementById('formcont')
+    if(cn.classList.contains('rotate')){
+        cn.classList.remove('rotate')
+    }else{
+        cn.classList.add('rotate')
+    }
+}
+const handleClick = ()=>{
+    var fname = document.getElementById('fname')
+    var lname = document.getElementById('lname')
+    var roll = document.getElementById('roll')
+    var age = document.getElementById('age')
+    var gender = document.getElementById('gen')
+    var hobby = document.getElementById('hobby')
+    const studentObj = {
+        firstName: fname.value,
+        lastLame: lname.value,
+        rollNumber: roll.value,
+        age: age.value,
+        gender: gender.value,
+        hobby: splitString(hobby.value)
+    }
+    sampleData.push(studentObj)
+    fname.value=""
+    lname.value=""
+    roll.value=""
+    age.value=""
+    gender.value=""
+    hobby.value=""
+    var element = document.getElementById('hobbyarray')
+    element.innerHTML=""
 }
 
 document.getElementById('submit').addEventListener('click',handleClick)
-
+document.getElementById('switch').addEventListener('click',handleSwitch)
+document.getElementById('switchback').addEventListener('click',handleSwitch)
+document.getElementById('go').addEventListener('click',handleSearch)
 });
---
+++ sampleData.js
@@ -0,0 +1,35 @@
+export const sampleData = [
+    {
+        firstName: "Anmol",
+        lastName: "Srivastava",
+        rollNumber: 1,
+        age: 22,
+        gender: "Male",
+        hobby: ["Piano", "Guitar"]
+    },
+    {
+        firstName: "Kamal",
+        lastName: "Kumar",
+        rollNumber: 2,
+        age: 22,
+        gender: "Male",
+        hobby: ["Dancing"]
+    },
+    {
+        firstName: "Ritik",
+        lastName: "Bhati",
+        rollNumber: 3,
+        age: 23,
+        gender: "Male",
+        hobby: []
+    },
+    {
+        firstName: "Vishal",
+        lastName: "Kumar",
+        rollNumber: 4,
+        age: 22,
+        gender: "Male",
+        hobby: ["Netflix and Chill", "BGMI"]
+    },
+
+]
