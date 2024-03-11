d8db612c49d74e11b7bb6a02eb1054618b87a3b1
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-11 07:50:38 +0530

    applied jquery snippets

--- main.js
+++ main.js
@@ -1,83 +1,76 @@
 import {sampleData} from "./sampleData.js";
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-  
-  const splitString = (string)=>{
-    var arr=string.split(',').map(item=>item.trim())
-    return arr.filter(item=>item !=="");
-  }
-  let test = document.getElementById('hobby')
-    test.addEventListener("input", (event) => {
-        let sample=event.target.value;
-        if(sample[sample.length-1]===','){
-            var element = document.getElementById('hobbyarray')
-            element.innerHTML=""
-            splitString(sample).forEach((value)=>{
-                var paragraph=document.createElement("p")
-                paragraph.textContent=value
-                element.appendChild(paragraph)
-            })
-        }
-    })
-    const fillResponse = (studentObj) => {
-        const responseDiv = document.getElementById('response');
-        responseDiv.innerHTML = '';
-        if(studentObj){
-            for (const [key, value] of Object.entries(studentObj)) {
-                const newDiv = document.createElement('div');
-                newDiv.textContent= `${key} : ${value}`
-                responseDiv.appendChild(newDiv)
-            }
-        }
-        else{
-            const newDiv = document.createElement('div');
-            newDiv.textContent= `No such entry found!!!!`
-            responseDiv.appendChild(newDiv)
-        }
-    }
-    
-const handleSearch=()=>{
-    let rollno=document.getElementById('searchname').value
-    const reqData = sampleData.find(student=> student.rollNumber==rollno)
-    console.log(reqData)
-    fillResponse(reqData)
-}
-const handleSwitch= ()=>{
-    let cn = document.getElementById('formcont')
-    if(cn.classList.contains('rotate')){
-        cn.classList.remove('rotate')
-    }else{
-        cn.classList.add('rotate')
-    }
-}
-const handleClick = ()=>{
-    var fname = document.getElementById('fname')
-    var lname = document.getElementById('lname')
-    var roll = document.getElementById('roll')
-    var age = document.getElementById('age')
-    var gender = document.getElementById('gen')
-    var hobby = document.getElementById('hobby')
-    const studentObj = {
-        firstName: fname.value,
-        lastLame: lname.value,
-        rollNumber: roll.value,
-        age: age.value,
-        gender: gender.value,
-        hobby: splitString(hobby.value)
-    }
-    sampleData.push(studentObj)
-    fname.value=""
-    lname.value=""
-    roll.value=""
-    age.value=""
-    gender.value=""
-    hobby.value=""
-    var element = document.getElementById('hobbyarray')
-    element.innerHTML=""
-}
 
-document.getElementById('submit').addEventListener('click',handleClick)
-document.getElementById('switch').addEventListener('click',handleSwitch)
-document.getElementById('switchback').addEventListener('click',handleSwitch)
-document.getElementById('go').addEventListener('click',handleSearch)
+$(document).ready(function() {
+    console.log("DOM fully loaded and parsed");
+
+    const splitString = (string) => {
+        var arr = string.split(',').map(item => item.trim());
+        return arr.filter(item => item !== "");
+    }
+
+    $('#hobby').on("input", function(event) {
+        let sample = $(this).val();
+        if (sample[sample.length - 1] === ',') {
+            var element = $('#hobbyarray');
+            element.empty();
+            splitString(sample).forEach(function(value) {
+                var paragraph = $('<p>').text(value);
+                element.append(paragraph);
+            });
+        }
+    });
+
+    const fillResponse = (studentObj) => {
+        const responseDiv = $('#response');
+        responseDiv.empty();
+        if (studentObj) {
+            for (const [key, value] of Object.entries(studentObj)) {
+                const newDiv = $('<div>').text(`${key} : ${value}`);
+                responseDiv.append(newDiv);
+            }
+        } else {
+            const newDiv = $('<div>').text(`No such entry found!!!!`);
+            responseDiv.append(newDiv);
+        }
+    }
+
+    const handleSearch = () => {
+        let rollno = $('#searchname').val();
+        const reqData = sampleData.find(student => student.rollNumber == rollno);
+        fillResponse(reqData);
+    }
+
+    const handleSwitch = () => {
+        $('#formcont').toggleClass('rotate');
+    }
+
+    const handleClick = () => {
+        var fname = $('#fname');
+        var lname = $('#lname');
+        var roll = $('#roll');
+        var age = $('#age');
+        var gender = $('#gen');
+        var hobby = $('#hobby');
+        const studentObj = {
+            firstName: fname.val(),
+            lastLame: lname.val(),
+            rollNumber: roll.val(),
+            age: age.val(),
+            gender: gender.val(),
+            hobby: splitString(hobby.val())
+        }
+        sampleData.push(studentObj);
+        fname.val("");
+        lname.val("");
+        roll.val("");
+        age.val("");
+        gender.val("");
+        hobby.val("");
+        $('#hobbyarray').empty();
+    }
+
+    $('#submit').click(handleClick);
+    $('#switch').click(handleSwitch);
+    $('#switchback').click(handleSwitch);
+    $('#go').click(handleSearch);
 });
